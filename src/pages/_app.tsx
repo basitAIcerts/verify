ca5b71341b430778dc6fe3d8b748fb691d408c18
Basit Channa <basit@aicerts@io>
2024-06-29 01:34:26 +0530

    IMAGE TO ENV

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
     const shareTitle = "Aicerts Certification";
     const shareDescription = "Aicerts Certification details.";
     const shareImage = "https://images.netcomlearning.com/ai-certs/cer365AllPageBg.png";
+    const apiUrl = process.env.NEXT_PUBLIC_BASE_URL_IMAGE;
   return (
     <>
       
@@ -18,15 +19,15 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content={shareTitle} />
                 <meta property="og:description" content={shareDescription} />
-                <meta property="og:image" content={"https://images.netcomlearning.com/ai-certs/cer365AllPageBg.png"} />
-                <meta property="og:image:secure_url" content={"https://images.netcomlearning.com/ai-certs/cer365AllPageBg.png"} />
+                <meta property="og:image" content={apiUrl} />
+                <meta property="og:image:secure_url" content={apiUrl} />
                 <meta property="og:image:type" content="image/png" />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content={shareTitle} />
                 <meta name="twitter:description" content={shareDescription} />
-                <meta name="twitter:image" content={"https://images.netcomlearning.com/ai-certs/cer365AllPageBg.png"} />
+                <meta name="twitter:image" content={apiUrl} />
                 <title>{shareTitle}</title>
                 <meta name="description" content={shareDescription} />
         <link rel="icon" href="https://images.netcomlearning.com/ai-certs/favIcon.svg" />
